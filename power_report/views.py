7d7d93e19e05dbb8d7eb078c11379abb648c2154
grupoecsa <52829864+grupoecsa@users.noreply.github.com>
2024-07-10 16:12:34 -0300

    Update views.py salidas

--- power_report/views.py
+++ power_report/views.py
@@ -24,17 +24,15 @@ class ReportForm(forms.Form):
 def power_report(request):
     return render(request, 'generate_report.html')
 
-
-    
 def cargar_archivo_csv(csv_file, pptx_template, no_process, nombre_evento, pase_nombre, fecha_evento, request):
     print("Procesando archivo CSV...")
     csv_file = request.FILES.get('csv_file', None)
-    pptx_template = request.FILES.get('pptx_template', None)  # Obtener la plantilla PPTX desde la solicitud
+    pptx_template = request.FILES.get('pptx_template', None)
     print("Archivo CSV:", csv_file)
     print("Plantilla PPTX:", pptx_template)
     
     if csv_file:
-        archivo_pptx = procesar_archivo_csv(csv_file, pptx_template, no_process, nombre_evento, pase_nombre, fecha_evento, request)  # Pasar los argumentos necesarios
+        archivo_pptx = procesar_archivo_csv(csv_file, pptx_template, no_process, nombre_evento, pase_nombre, fecha_evento, request)
         if isinstance(archivo_pptx, HttpResponse):
             return archivo_pptx
         else:
@@ -45,11 +43,10 @@ def cargar_archivo_csv(csv_file, pptx_template, no_process, nombre_evento, pase_
     else:
         return HttpResponse("Error: No se proporcionó ningún archivo CSV.")
 
-    
-def procesar_archivo_csv(csv_file, pptx_template , no_process , nombre_evento, pase_nombre, fecha_evento, request):
+def procesar_archivo_csv(csv_file, pptx_template, no_process, nombre_evento, pase_nombre, fecha_evento, request):
     print("Procesando archivo CSV...")
     if pptx_template:
-        prs = Presentation(pptx_template)  # Usar la plantilla PPTX proporcionada
+        prs = Presentation(io.BytesIO(pptx_template.read()))  # Leer la plantilla PPTX proporcionada desde memoria
     else:
         prs = Presentation()  # Si no se proporciona una plantilla, crear una presentación vacía
 
@@ -59,23 +56,22 @@ def procesar_archivo_csv(csv_file, pptx_template , no_process , nombre_evento, p
     if len(lines) < 3:
         return HttpResponse("Error: El archivo CSV no tiene suficientes líneas.")
 
-    no_process = request.POST.get('no_process', None)
-    nombre_evento = request.POST.get('nombre_evento', None)
-    pase_nombre = request.POST.get('pase_nombre', None)
-    fecha_evento = request.POST.get('fecha_evento', None)
-
     reader = csv.DictReader(lines, delimiter=';')
 
     puertas_unicas = {}
     categorias_unicas = {}
     suma_tipo_acceso = 0
-    fechas_grafico = []  # Inicializar la variable aquí
-    entradas = []  # Inicializar la variable aquí
-    salidas = []  # Inicializar la variable aquí
+    fechas_grafico = []
+    entradas = []
+    salidas = []
+
     try:
         for row in reader:
             puerta = row['Puerta']
             tipo_acceso_str = row['Tipo acceso']
+
+            if no_process and tipo_acceso_str == "Salida":
+                continue
 
             if puerta not in puertas_unicas:
                 puertas_unicas[puerta] = 0
@@ -96,7 +92,6 @@ def procesar_archivo_csv(csv_file, pptx_template , no_process , nombre_evento, p
             while len(prs.slides) < 3:
                 prs.slides.add_slide(prs.slide_layouts[0])
             slide3 = prs.slides[2]
-        # Resto del código para el slide 3
 
         if len(prs.slides) >= 4:
             slide4 = prs.slides[3]
@@ -104,7 +99,6 @@ def procesar_archivo_csv(csv_file, pptx_template , no_process , nombre_evento, p
             while len(prs.slides) < 4:
                 prs.slides.add_slide(prs.slide_layouts[0])
             slide4 = prs.slides[3]
-        # Resto del código para el slide 4
 
         if len(prs.slides) >= 5:
             slide5 = prs.slides[4]
@@ -112,7 +106,6 @@ def procesar_archivo_csv(csv_file, pptx_template , no_process , nombre_evento, p
             while len(prs.slides) < 5:
                 prs.slides.add_slide(prs.slide_layouts[0])
             slide5 = prs.slides[4]
-        # Resto del código para el slide 5
 
         slide_width = Inches(10)
         slide_height = Inches(5.625)
@@ -177,59 +170,54 @@ def procesar_archivo_csv(csv_file, pptx_template , no_process , nombre_evento, p
             with open(temp_file_path, "r", encoding="latin1") as file:
                 for _ in range(0):
                     next(file)
-                print(file)
                 reader = csv.DictReader(file, delimiter=';')
                 fechas = []
                 tipo_acceso = []
                 for row in reader:
                     fecha_str = row.get('Fecha', None)
                     tipo_acceso_str = row.get('Tipo acceso', None)
+                    if no_process and tipo_acceso_str == "Salida":
+                        continue
                     if fecha_str and tipo_acceso_str and (tipo_acceso_str == "Entrada" or tipo_acceso_str == "Salida"):
                         try:
                             fecha = datetime.strptime(fecha_str, "%d/%m/%Y %H:%M:%S")
                             fechas.append(fecha)
                             tipo_acceso.append(tipo_acceso_str)
-                          # Contar las entradas y salidas
                             if tipo_acceso_str == "Entrada":
-                               entradas.append(fecha)  # Agregar la fecha de entrada
+                                entradas.append(fecha)
                             elif tipo_acceso_str == "Salida" and not no_process:
-                               salidas.append(fecha)  # Agregar la fecha de salida
-
+                                salidas.append(fecha)
                         except ValueError as e:
                             print(f"Error: {e}")
                 if fechas:
                     fecha_inicial = min(fechas)
                     fecha_final = max(fechas)
-                    fechas_grafico = [fecha_inicial + timedelta(minutes=30 * i) for i in
-                                      range(int((fecha_final - fecha_inicial).total_seconds() / 1800) + 1)]
+                    fechas_grafico = [fecha_inicial + timedelta(minutes=30 * i) for i in range(int((fecha_final - fecha_inicial).total_seconds() / 1800) + 1)]
                     entradas = [0] * len(fechas_grafico)
                     salidas = [0] * len(fechas_grafico)
                     for i, fecha in enumerate(fechas_grafico):
-                         for j, fecha_csv in enumerate(fechas):
-                              if fecha <= fecha_csv < fecha + timedelta(minutes=30):
+                        for j, fecha_csv in enumerate(fechas):
+                            if fecha <= fecha_csv < fecha + timedelta(minutes=30):
                                 if tipo_acceso[j] == "Entrada":
-                                   entradas[i] += 1
+                                    entradas[i] += 1
                                 elif tipo_acceso[j] == "Salida":
-                                   salidas[i] += 1
-
+                                    salidas[i] += 1
                 else:
-                    # Mostrar un mensaje de error o registrar la falta de fechas válidas
                     print("No se encontraron fechas válidas en el archivo CSV.")
                 print("Datos del gráfico:")
                 print("Fechas:", fechas_grafico)
                 print("Entradas:", entradas)
                 print("Salidas:", salidas)
-
         except ValueError as e:
             print(f"Error: {e}")
         finally:
             if temp_file_path:
                 os.unlink(temp_file_path)
 
-# Resto del código para agregar el gráfico a la presentación
         with io.BytesIO() as image_stream:
             plt.plot(fechas_grafico, entradas, color='orange', label='Entradas')
-            plt.plot(fechas_grafico, salidas, color='skyblue', label='Salidas')
+            if not no_process:
+                plt.plot(fechas_grafico, salidas, color='skyblue', label='Salidas')
             plt.xlabel('Fecha y Hora')
             plt.ylabel('Cantidad')
             plt.title('Ingresos y Salidas por Intervalo de 30 minutos')
@@ -246,7 +234,6 @@ def procesar_archivo_csv(csv_file, pptx_template , no_process , nombre_evento, p
             print("El gráfico se ha agregado correctamente a la presentación de PowerPoint.")
 
             plt.close()
-        # Resto del código para actualizar los placeholders en la presentación PowerPoint
 
         for slide in prs.slides:
             for shape in slide.shapes:
@@ -258,10 +245,9 @@ def procesar_archivo_csv(csv_file, pptx_template , no_process , nombre_evento, p
                             elif run.text == "{{PASE_NOMBRE}}":
                                 run.text = pase_nombre
                             elif run.text == "{{FECHA_EVENTO}}":
-                                run.text = fecha_evento
+                                run.text = str(fecha_evento)
                             elif run.text == "{{INGRESO_NUMERO}}":
                                 run.text = str(suma_tipo_acceso)
-        # Guardar la presentación PowerPoint en memoria y retornarla como respuesta HTTP
 
         pptx_in_memory = io.BytesIO()
         prs.save(pptx_in_memory)
@@ -284,10 +270,9 @@ def generate_report(request):
             pase_nombre = form.cleaned_data['pase_nombre']
             fecha_evento = form.cleaned_data['fecha_evento']
             archivo_pptx = cargar_archivo_csv(csv_file, pptx_template, no_process, nombre_evento, pase_nombre, fecha_evento, request)
-            # Resto del código
-        if isinstance(archivo_pptx, HttpResponse):
-            return archivo_pptx
-        else:
-            return render(request, 'generate_report.html', {'informe_generado': archivo_pptx})
+            if isinstance(archivo_pptx, HttpResponse):
+                return archivo_pptx
+            else:
+                return render(request, 'generate_report.html', {'informe_generado': archivo_pptx})
     else:
         return render(request, 'generate_report.html')
